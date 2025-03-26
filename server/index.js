c2a3bab64a4b3939ce4c22d86d9179255fd727c3
AyaanZaidi <ayaanzaidi006@gmali.com>
2025-03-26 21:37:09 +0500

    Fixed login API issue

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -13,7 +14,8 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-mongoose.connect("mongodb+srv://akifking559:dbConnection@cluster0.2qqj2ns.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
