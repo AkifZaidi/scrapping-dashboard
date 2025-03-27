1e3f26545eddbe01dad4d82c6a2fbaf18603cd50
AyaanZaidi <ayaanzaidi006@gmali.com>
2025-03-27 17:44:53 +0500

    fix all bugs

--- server/index.js
+++ server/index.js
@@ -23,9 +23,14 @@ app.options("*", cors());
 app.use(cookieParser());
 
 
-mongoose.connect(process.env.MONGO_URI,)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000, // 10 seconds timeout
+})
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.error('Error connecting to MongoDB:', error));
+
 
 // Home route
 app.get("/", (req, res) => {
