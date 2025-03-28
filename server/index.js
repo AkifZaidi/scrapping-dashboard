873ffd8fa90147b249c8fbf665db64a277ced637
AyaanZaidi <ayaanzaidi006@gmali.com>
2025-03-28 15:33:23 +0500

    fix all bugs

--- server/index.js
+++ server/index.js
@@ -19,14 +19,6 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
-
 app.options("*", cors());
 
 
